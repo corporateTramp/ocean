060e6242b771e53d591f960544ffd6d1d556975f
Alexander Kryuchkov <alex.s.kryuchkov@gmail.com>
2016-02-19 01:21:48 +0300

    v1.4

--- inst.py
+++ inst.py
@@ -11,7 +11,6 @@ def collect():
 	pages  = []
 	r = requests.get('http://www.t30p.ru/Instagram.aspx')
 	soup = BeautifulSoup(r.text, 'html5lib')
-	# print r.text
 	
 	for tag in soup.find_all('td',class_="name"):
 		page = tag.a.get('href')
@@ -22,48 +21,57 @@ def collect():
 
 
 def instagram(urls):
-
-	for url in urls:
+	
+	accounts =[]
+	scan_sessions = []
+	content_params =[]
+	
+	for url in urls[:10]:
 		# time.sleep(7)
 		driver = webdriver.PhantomJS()
 		driver.get(url)
-
-
-		user = driver.find_element_by_xpath("//section/main/article/header/div[2]/div[1]/h1")
-		desc = driver.find_element_by_xpath ("//section/main/article/header/div[2]/div[2]/span[2]")
-		posts = driver.find_element_by_xpath ("//section/main/article/ul/li[1]/span/span[2]")
-		followers = driver.find_element_by_xpath ("//section/main/article/ul/li[2]/span/span[2]")
-
-			
-		print user.text
-		print desc.text
-		print posts.text
-		print followers.text
 		
 		try:
 			for x in range (1,5):
 				for i in range(1,4):
+					private = False
 					pic = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div"%(x,i))
 					hover = ActionChains(driver).move_to_element(pic)
 					hover.perform()
 					try:
-						likes = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[2]/ul/li/span[2]" %(x,i))
-						comments = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[2]/ul/li[2]/span[2]" %(x,i))
-						type = "Photo"
+						likes = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[2]/ul/li/span[2]" %(x,i)).text
+						comments = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[2]/ul/li[2]/span[2]" %(x,i)).text
+						content_type = "photo"
 						alt = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[1]/div[1]/img" %(x,i))
 					except:
-						likes = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[3]/ul/li/span[2]" %(x,i))
-						comments = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[3]/ul/li[2]/span[2]" %(x,i))
-						type = "Video"
-						alt = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[1]/div[1]/img" %(x,i))
-					print likes.text
-					print comments.text
-					print type
-					print alt.get_attribute("alt")
+						likes = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[3]/ul/li/span[2]" %(x,i)).text
+						comments = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[3]/ul/li[2]/span[2]" %(x,i)).text
+						content_type = "video"
+						alt = driver.find_element_by_xpath("//section/main/article/div[1]/div/div[%d]/a[%d]/div[1]/div[1]/img" %(x,i)).get_attribute("alt")
+						
+				content_params_add = {"name": name, "content_type": content_type, "description": alt, "likes":likes,"comments":comments}
+				content_params.append(content_params_add)
+
 		except NoSuchElementException:
-					print "invisible page"
-				
+					private = True
+					
+		name = driver.find_element_by_xpath("//section/main/article/header/div[2]/div[1]/h1").text
+		description = driver.find_element_by_xpath ("//section/main/article/header/div[2]/div[2]/span[2]").text
+		publications = driver.find_element_by_xpath ("//section/main/article/ul/li[1]/span/span[2]").text
+		subscribers = driver.find_element_by_xpath ("//section/main/article/ul/li[2]/span/span[2]").text
+		subscribtions = driver.find_element_by_xpath ("//section/main/article/ul/li[3]/span/span[2]").text
+		
+		accounts_add = {"name": name, "description": description, "private": private}
+		scan_sessions_add = {"name": name, "publications": publications, "subscribers": subscribers, "subscribtions": subscribtions}
+		
+		accounts.append(accounts_add)
+		scan_sessions.append(scan_sessions_add)
+	
 		driver.quit()
+		
+	print accounts, scan_sessions, content_params
+		
+		
 				
 
 urls = collect()			
