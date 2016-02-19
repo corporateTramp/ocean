f37533760b0c260b343edc2b814d0c164e1f24a5
Alexander Kryuchkov <alex.s.kryuchkov@gmail.com>
2016-02-19 18:49:44 +0300

    v1.5

--- ch_tables.py
+++ ch_tables.py
@@ -6,18 +6,18 @@ cur = conn.cursor()
 print "ACCOUNTS----------------------------------------"
 cur = conn.cursor()
 cur.execute("SELECT * FROM accounts;")
-cur.fetchall()
+print cur.fetchall()
 cur.close()
 print "SCAN_SESSEIONS----------------------------------------"
 cur = conn.cursor()
 cur.execute("SELECT * FROM scan_sessions;")
-cur.fetchall()
+print cur.fetchall()
 cur.close()
 
 print "CONTENT_PARAMS----------------------------------------"
 cur = conn.cursor()
 cur.execute("SELECT * FROM content_params;")
-cur.fetchall()
+print cur.fetchall()
 cur.close()
 
 conn.close()
