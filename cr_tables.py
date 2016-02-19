c41324bb0f18bf9040d0ca308b5adf5e3732da93
Alexander Kryuchkov <alex.s.kryuchkov@gmail.com>
2016-02-19 19:47:53 +0300

    v1.5

--- cr_tables.py
+++ cr_tables.py
@@ -5,7 +5,7 @@ cur = conn.cursor()
 
 cur.execute("CREATE TABLE accounts ( id SERIAL PRIMARY KEY, name character(150), description text, private bool, created_at timestamp, updated_at timestamp);")
 
-cur.execute("CREATE TABLE scan_sessions ( id SERIAL PRIMARY KEY, account_id integer, publications integer,subscribers integer, subscribtions integer, created_at timestamp);")
+cur.execute("CREATE TABLE scan_sessions ( id SERIAL PRIMARY KEY, account_id integer, publications text,subscribers text, subscribtions text, created_at timestamp);")
 
 cur.execute("CREATE TABLE content_params ( id SERIAL PRIMARY KEY, account_id integer, scan_session_id integer,content_type character(10),description text, likes text, comments text, created_at timestamp);")
 
