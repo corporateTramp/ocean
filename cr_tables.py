f3f6a833b400d07500d69577d85382389731b691
Alexander Kryuchkov <alex.s.kryuchkov@gmail.com>
2016-02-19 17:54:17 +0300

    v1.5

--- cr_tables.py
+++ cr_tables.py
@@ -7,12 +7,13 @@ cur.execute("CREATE TABLE accounts ( id SERIAL PRIMARY KEY, name character(150),
 
 cur.execute("CREATE TABLE scan_sessions ( id SERIAL PRIMARY KEY, account_id integer, publications integer,subscribers integer, subscribtions integer, created_at timestamp);")
 
-cur.execute("CREATE TABLE content_params ( id SERIAL PRIMARY KEY, account_id integer, scan_session_id integer,content_type character(10),description text, likes integer, comments integer, created_at timestamp);")
+# cur.execute("CREATE TABLE content_params ( id SERIAL PRIMARY KEY, account_id integer, scan_session_id integer,content_type character(10),description text, likes integer, comments integer, created_at timestamp);")
 
 conn.commit()
+cur.close()
 
+cur = conn.cursor()
 cur.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
 print cur.fetchall()
 
-cur.close()
 conn.close()
--- del_tables.py
+++ del_tables.py
@@ -4,10 +4,10 @@ conn = psycopg2.connect("dbname=postgres user=postgres password =postgres" )
 cur = conn.cursor()
 cur.execute ("DROP TABLE IF EXISTS accounts, scan_sessions, content_params")
 conn.commit()
+cur.close()
 
+cur = conn.cursor()
 cur.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
 print cur.fetchall()
 
-cur.close()
 conn.close()
-
