012c7a8cde69b2d046e36d4d0b4b6725c4deec99
Alexander Kryuchkov <alex.s.kryuchkov@gmail.com>
2016-02-20 05:43:29 +0300

    v1.6

--- instagram_parser.py
+++ instagram_parser.py
@@ -283,11 +283,11 @@ def refresh_tables(db_data="dbname=postgres user=postgres password =postgres"):
 	delete_tables (db_data)
 	create_tables(db_data)
 	
-def see_table(table = accounts, db_data="dbname=postgres user=postgres password =postgres"):
+def see_table(table = 'accounts', field = '*' db_data="dbname=postgres user=postgres password =postgres"):
 	conn = psycopg2.connect(db_data)
 	cur = conn.cursor()
 	print "---------------------------------------------------------------------------------------"
-	cur.execute("SELECT * FROM %s;", (table,))
+	cur.execute("SELECT %s FROM %s;", (field, table,))
 	print cur.fetchall()
 	cur.close()
 	conn.close()
