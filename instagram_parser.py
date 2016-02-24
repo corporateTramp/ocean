e474eea8babea6db8642fb764b7927f443af03b4
Alexander Kryuchkov <alex.s.kryuchkov@gmail.com>
2016-02-24 23:41:46 +0300

    v1.9

--- instagram_parser.py
+++ instagram_parser.py
@@ -105,9 +105,9 @@ def start_init(urls, wait, db_data="dbname=Localhosts user=postgres password=pos
 						
 						content_params_new = [content_type, alt, likes, comments]
 						content_params_add.append(content_params_new)
-						
 						content_params_new = [content_type, alt, likes, comments]
 						content_params_add.append(content_params_new)
+					except:
 			
 			##filtering of what to add and updating accounts
 			account_add = (name, description, private)	
