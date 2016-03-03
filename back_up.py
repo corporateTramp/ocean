57a9d973f7746a2e0f4b84b7dba92edfd1387c0f
corporateTramp <alex.s.kryuchkov@gmail.com>
2016-03-04 01:42:57 +0300

    Update back_up.py

--- back_up.py
+++ back_up.py
@@ -95,7 +95,7 @@ def start_init(urls, wait, db_data="dbname=alex user=alex password=alexdb"):
 			#parsing posts
 			content_params_add =[]
 			if private == False:
-				for i in range (0,9):
+				for i in range (0,10):
 					try:
 						isVideo = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["is_video"]
 						if isVideo == False:
