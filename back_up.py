eda12b9410780b9ca8a3d1d8235e7c13c79ac604
corporateTramp <alex.s.kryuchkov@gmail.com>
2016-03-04 01:27:43 +0300

    Update back_up.py

--- back_up.py
+++ back_up.py
@@ -146,6 +146,7 @@ def start_init(urls, wait, db_data="dbname=alex user=alex password=alexdb"):
 			if name == updatedAccounts[i][1].decode('utf-8'):
 				account_id = updatedAccounts[i][0]
 		
+		print likes_sum/10, likes_sum/subscribers, publications/subscribers
 		scan_sessions_add = (account_id, publications, subscribers, subscriptions, likes_sum/10, likes_sum/subscribers, publications/subscribers)
 		cur = conn.cursor()
 		cur.execute("INSERT INTO scan_sessions(account_id, publications, subscribers, subscriptions, created_at, active_subscribers, er, avg) VALUES (%s, %s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP), %s, %s, %s);", scan_sessions_add)
@@ -185,7 +186,7 @@ def create_tables(db_data="dbname=alex user=alex password=alexdb"):
 	cur = conn.cursor()
 
 	cur.execute("CREATE TABLE accounts ( id SERIAL PRIMARY KEY, name text, description text, private bool, created_at timestamp, updated_at timestamp);")
-	cur.execute("CREATE TABLE scan_sessions ( id SERIAL PRIMARY KEY, account_id integer, publications integer,subscribers integer, subscriptions integer, created_at timestamp, active_subscribers decimal(4) , er decimal(4) , avg decimal(4) );")
+	cur.execute("CREATE TABLE scan_sessions ( id SERIAL PRIMARY KEY, account_id integer, publications integer,subscribers integer, subscriptions integer, created_at timestamp, active_subscribers real , er real , avg real );")
 	cur.execute("CREATE TABLE content_params ( id SERIAL PRIMARY KEY, account_id integer, scan_session_id integer,content_type text,description text, likes integer, comments integer, created_at timestamp);")
 
 	conn.commit()
