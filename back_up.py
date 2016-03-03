d994ae3f318c75058bcb22f98da93082b06c12e1
corporateTramp <alex.s.kryuchkov@gmail.com>
2016-03-04 01:04:08 +0300

    Update back_up.py

--- back_up.py
+++ back_up.py
@@ -52,137 +52,129 @@ def start_init(urls, wait, db_data="dbname=alex user=alex password=alexdb"):
 		
 		print "Started: " + url + " (" + str(counter) +")"
 		counter = counter + 1
-		try:
-			
-			r = requests.get(url, headers=custom_headers)
-			soup = BeautifulSoup(r.text, 'html5lib')
-			json_data = soup.body.find_all(type="text/javascript")
-			json_data = json_data[0].text
-			json_data = json_data[21:(len(json_data)-1)]
-			
-			data = json.loads(json_data)
-			
-			#parsing main info
-			private = data["entry_data"]["ProfilePage"][0]["user"]["is_private"]
-			publications = data["entry_data"]["ProfilePage"][0]["user"]["media"]["count"]
-			name = data["entry_data"]["ProfilePage"][0]["user"]["username"]
-			subscribers = data["entry_data"]["ProfilePage"][0]["user"]["followed_by"]["count"]
-			subscriptions = data["entry_data"]["ProfilePage"][0]["user"]["follows"]["count"]
-			try:
-				fullName = data["entry_data"]["ProfilePage"][0]["user"]["full_name"]
-			except:
-				fullName = " "
-			
-			try:
-				bio = data["entry_data"]["ProfilePage"][0]["user"]["biography"]
-			except:
-				bio = " "
-				
-			try:
-				external_url = data["entry_data"]["ProfilePage"][0]["user"]["external_url"]
-			except:
-				external_url = " "
-
-			if not fullName: fullName = ""
-			if not bio: bio = ""
-			if not external_url: external_url = ""
-
-			description = fullName + " " + bio + " " + external_url
-			
-			likes_sum = 0
-			
-			#parsing posts
-			content_params_add =[]
-			if private == False:
-				for i in range (0,9):
-					try:
-						isVideo = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["is_video"]
-						if isVideo == False:
-							content_type = "Photo"
-						else:
-							content_type = "Video"
-						likes = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["likes"]["count"]
-						comments = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["comments"]["count"]
-						try:
-							alt = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["caption"]
-						except:
-							alt = ""	
-						
-						content_params_new = [content_type, alt, likes, comments]
-						content_params_add.append(content_params_new)
-						
-						likes_sum = likes_sum + int(likes)
-					except:
-						pass
-			
-			##filtering of what to add and updating accounts
-			account_add = (name, description, private)
-			cur = conn.cursor()
-			flag = 0
-			
-			for acc in range(0,len(begAccounts)):
-				if name == (convert_tuple_to_unicode(begAccounts[acc]))[0]:
-					cur.execute("UPDATE accounts SET (name, description, private, updated_at) = (%s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP)) WHERE name = %s ;", (name, description, private, name,))
-					conn.commit()
-					flag = 1
-					
-			if flag == 0:
-					cur.execute("INSERT INTO accounts(name, description, private, created_at) VALUES (%s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP));", account_add)
-					conn.commit()
-					
-			cur.close()
-					
-			##mapping account id and updating scan_sessions
-			cur = conn.cursor()
-			cur.execute("SELECT id, name FROM accounts;")
-			updatedAccounts  = cur.fetchall()
-			updatedAccounts = convert_tuple_to_unicode(updatedAccounts)
-			cur.close()
-			
-			account_id = 999999
-			for i in range(0,len(updatedAccounts)):
-				if name == updatedAccounts[i][1].decode('utf-8'):
-					account_id = updatedAccounts[i][0]
-			
-			scan_sessions_add = (account_id, publications, subscribers, subscriptions, likes_sum/10, likes_sum/subscribers, publications/subscribers)
-			cur = conn.cursor()
-			cur.execute("INSERT INTO scan_sessions(account_id, publications, subscribers, subscriptions, created_at, active_subscribers, er, avg) VALUES (%s, %s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP), %s, %s, %s);", scan_sessions_add)
-			conn.commit()
-			cur.close()
-			
-			
-			##mapping scan_id
-			cur = conn.cursor()
-			cur.execute("SELECT id, account_id FROM scan_sessions;")
-			updatedSessions  = cur.fetchall()
-			cur.close()
-			
-			scan_session_id = 999999
-			for q in range(0,len(updatedSessions)):
-				if account_id == updatedSessions[q][1]:
-					scan_session_id = updatedSessions[q][0]
-					
-			##adding content params
-			for w in range(0,len(content_params_add)):
-				content_params_add[w].insert(0,scan_session_id)
-				content_params_add[w].insert(0,account_id)			
-			
-			content_params_add = [tuple(l) for l in content_params_add]
-			cur = conn.cursor()
-			cur.executemany("INSERT INTO content_params (account_id, scan_session_id, content_type, description, likes, comments, created_at) VALUES (%s, %s, %s, %s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP));", content_params_add)
-			conn.commit()
-			cur.close()
-
-			time.sleep(wait)
 		
-		except urllib2.HTTPError, err:
-			if err.code == 403:
-				time.sleep(20)
-				print "Instagram denied access to", url
-			else:
-				print "Connection problem raised on", url
+			
+		r = requests.get(url, headers=custom_headers)
+		soup = BeautifulSoup(r.text, 'html5lib')
+		json_data = soup.body.find_all(type="text/javascript")
+		json_data = json_data[0].text
+		json_data = json_data[21:(len(json_data)-1)]
+		
+		data = json.loads(json_data)
+		
+		#parsing main info
+		private = data["entry_data"]["ProfilePage"][0]["user"]["is_private"]
+		publications = data["entry_data"]["ProfilePage"][0]["user"]["media"]["count"]
+		name = data["entry_data"]["ProfilePage"][0]["user"]["username"]
+		subscribers = data["entry_data"]["ProfilePage"][0]["user"]["followed_by"]["count"]
+		subscriptions = data["entry_data"]["ProfilePage"][0]["user"]["follows"]["count"]
+		try:
+			fullName = data["entry_data"]["ProfilePage"][0]["user"]["full_name"]
 		except:
-			print "Exception on link: ", url
+			fullName = " "
+		
+		try:
+			bio = data["entry_data"]["ProfilePage"][0]["user"]["biography"]
+		except:
+			bio = " "
+			
+		try:
+			external_url = data["entry_data"]["ProfilePage"][0]["user"]["external_url"]
+		except:
+			external_url = " "
+
+		if not fullName: fullName = ""
+		if not bio: bio = ""
+		if not external_url: external_url = ""
+
+		description = fullName + " " + bio + " " + external_url
+		
+		likes_sum = 0
+		
+		#parsing posts
+		content_params_add =[]
+		if private == False:
+			for i in range (0,9):
+				try:
+					isVideo = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["is_video"]
+					if isVideo == False:
+						content_type = "Photo"
+					else:
+						content_type = "Video"
+					likes = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["likes"]["count"]
+					comments = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["comments"]["count"]
+					try:
+						alt = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"][i]["caption"]
+					except:
+						alt = ""	
+					
+					content_params_new = [content_type, alt, likes, comments]
+					content_params_add.append(content_params_new)
+					
+					likes_sum = likes_sum + int(likes)
+				except:
+					pass
+		
+		##filtering of what to add and updating accounts
+		account_add = (name, description, private)
+		cur = conn.cursor()
+		flag = 0
+		
+		for acc in range(0,len(begAccounts)):
+			if name == (convert_tuple_to_unicode(begAccounts[acc]))[0]:
+				cur.execute("UPDATE accounts SET (name, description, private, updated_at) = (%s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP)) WHERE name = %s ;", (name, description, private, name,))
+				conn.commit()
+				flag = 1
+				
+		if flag == 0:
+				cur.execute("INSERT INTO accounts(name, description, private, created_at) VALUES (%s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP));", account_add)
+				conn.commit()
+				
+		cur.close()
+				
+		##mapping account id and updating scan_sessions
+		cur = conn.cursor()
+		cur.execute("SELECT id, name FROM accounts;")
+		updatedAccounts  = cur.fetchall()
+		updatedAccounts = convert_tuple_to_unicode(updatedAccounts)
+		cur.close()
+		
+		account_id = 999999
+		for i in range(0,len(updatedAccounts)):
+			if name == updatedAccounts[i][1].decode('utf-8'):
+				account_id = updatedAccounts[i][0]
+		
+		scan_sessions_add = (account_id, publications, subscribers, subscriptions, likes_sum/10, likes_sum/subscribers, publications/subscribers)
+		cur = conn.cursor()
+		cur.execute("INSERT INTO scan_sessions(account_id, publications, subscribers, subscriptions, created_at, active_subscribers, er, avg) VALUES (%s, %s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP), %s, %s, %s);", scan_sessions_add)
+		conn.commit()
+		cur.close()
+		
+		
+		##mapping scan_id
+		cur = conn.cursor()
+		cur.execute("SELECT id, account_id FROM scan_sessions;")
+		updatedSessions  = cur.fetchall()
+		cur.close()
+		
+		scan_session_id = 999999
+		for q in range(0,len(updatedSessions)):
+			if account_id == updatedSessions[q][1]:
+				scan_session_id = updatedSessions[q][0]
+				
+		##adding content params
+		for w in range(0,len(content_params_add)):
+			content_params_add[w].insert(0,scan_session_id)
+			content_params_add[w].insert(0,account_id)			
+		
+		content_params_add = [tuple(l) for l in content_params_add]
+		cur = conn.cursor()
+		cur.executemany("INSERT INTO content_params (account_id, scan_session_id, content_type, description, likes, comments, created_at) VALUES (%s, %s, %s, %s, %s, %s, date_trunc('second',CURRENT_TIMESTAMP));", content_params_add)
+		conn.commit()
+		cur.close()
+
+		time.sleep(wait)
+		
 		
 	conn.close()
 
